09e9589b0d49cac0db7766fb27ef3e92fbf3a47b
Eliel <elielmichel@gmail.com>
2022-09-13 22:04:15 -0300

    feat(): create project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Kernel game = new Kernel();
+
+        game.printCave();
+        game.walk();
+        game.walk();
+        game.printCave();
+    }
+}
